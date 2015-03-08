b1380a2506d4dc492401eea25c573dab275e43f1
Zavire Shiran <zavire.shiran@gmail.com>
2015-03-08 03:03:43 -0500

    Add primitive entity rendering.

--- raider.lisp
+++ raider.lisp
@@ -16,21 +16,29 @@
 
 (defclass dungeon ()
   ((terrain :accessor terrain :initarg :terrain)
-   (entities :accessor entities :initarg :entities)))
+   (entities :accessor entities :initarg :entities :initform nil)))
 
 (defun make-dungeon (size)
   (let ((array (make-array size)))
     (loop for x from 0 to (- (first size) 1)
-	 do (loop for y from 0 to (- (second size) 1)
-		 do (setf (aref array x y) (make-instance 'terrain))))
+         do (loop for y from 0 to (- (second size) 1)
+                 do (setf (aref array x y) (make-instance 'terrain))))
     (make-instance 'dungeon :terrain array)))
 
+(defun find-entity (entities x y)
+  (let ((entity-here nil))
+    (dolist (entity entities entity-here)
+      (let ((p (pos entity)))
+	(if (and (= (first p) x) (= (second p) y))
+	    (setf entity-here entity))))))
+
 (defun display-dungeon (dungeon)
-  (with-slots (terrain) dungeon
+  (with-slots (terrain entities) dungeon
     (destructuring-bind (xsize ysize) (array-dimensions terrain)
       (loop for x from 0 to (- xsize 1)
-	   do (loop for y from 0 to (- ysize 1)
-		   do (if (passable (aref terrain x y))
-			  (format t ".")
-			  (format t "O")))
-	      (format t "~%")))))
+           do (loop for y from 0 to (- ysize 1)
+                   do (let ((e (find-entity entities x y)))
+			(cond (e (format t "A"))
+			      ((passable (aref terrain x y)) (format t "."))
+			      (t (format t "O")))))
+              (format t "~%")))))
