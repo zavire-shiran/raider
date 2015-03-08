44ab124e2c53ca506219b6338d6c81d20e47943d
Zavire Shiran <zavire.shiran@gmail.com>
2015-03-08 12:54:36 -0500

    Add behaviors, resolve turn-function, and position struct.

--- raider.lisp
+++ raider.lisp
@@ -3,6 +3,8 @@
 
 (in-package #:raider)
 
+(defstruct (pos (:conc-name pos-)) x y)
+
 (defclass entity ()
   ((name :accessor name :initarg :name)
    (display-char :accessor dchar :initarg :display-char :initform nil)
@@ -17,7 +19,8 @@
 (defclass arena ()
   ((terrain :accessor terrain :initarg :terrain)
    (entities :accessor entities :initarg :entities :initform nil)
-   (behaviors :accessor behaviors :initarg :behaviors :initform nil)))
+   (behaviors :accessor behaviors :initarg :behaviors :initform nil)
+   (turn-order :accessor turn-order :initarg :turn-order :initform nil)))
 
 (defclass ability ()
   ((name :accessor name :initarg :name)
@@ -37,7 +40,7 @@
   (let ((item (assoc team (behaviors arena))))
     (if item
         (setf (cdr item) behavior)
-        (setf (behavior arena) (acons team behavior (behavior arena))))))
+        (setf (behaviors arena) (acons team behavior (behaviors arena))))))
 
 (defun add-entity (arena entity)
   (setf (entities arena) (cons entity (entities arena))))
@@ -46,14 +49,14 @@
   (let ((entity-here nil))
     (dolist (entity entities entity-here)
       (let ((p (pos entity)))
-        (if (and (= (first p) x) (= (second p) y))
+        (if (and (= (pos-x p) x) (= (pos-y p) y))
             (setf entity-here entity))))))
 
 (defun display-arena (arena)
   (with-slots (terrain entities) arena
     (destructuring-bind (xsize ysize) (array-dimensions terrain)
-      (loop for x from 0 to (- xsize 1)
-           do (loop for y from 0 to (- ysize 1)
+      (loop for y from (- ysize 1) downto 0
+           do (loop for x from 0 to (- xsize 1)
                    do (let ((e (find-entity entities x y)))
                         (cond (e (if (dchar e)
                                      (format t "~a" (dchar e))
@@ -70,49 +73,89 @@
     (add-entity arena (make-instance 'entity
                                      :name "hero1"
                                      :team 'hero
-                                     :position (list 0 0)
+                                     :position (make-pos :x 0 :y 0)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero2"
                                      :team 'hero
-                                     :position (list 1 0)
+                                     :position (make-pos :x 1 :y 0)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero3"
                                      :team 'hero
-                                     :position (list 0 1)
+                                     :position (make-pos :x 0 :y 1)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero4"
                                      :team 'hero
-                                     :position (list 1 1)
+                                     :position (make-pos :x 1 :y 1)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
+
     (add-entity arena (make-instance 'entity
                                      :name "monster1"
                                      :team 'monster
                                      :abilities (list (make-ability))
                                      :display-char "M"
-                                     :position (list 9 9)))
+                                     :position (make-pos :x 9 :y 9)))
     (add-entity arena (make-instance 'entity
                                      :name "monster2"
                                      :team 'monster
                                      :abilities (list (make-ability))
                                      :display-char "M"
-                                     :position (list 8 9)))
+                                     :position (make-pos :x 9 :y 8)))
     (add-entity arena (make-instance 'entity
                                      :name "monster3"
                                      :team 'monster
                                      :abilities (list (make-ability))
                                      :display-char "M"
-                                     :position (list 9 8)))
+                                     :position (make-pos :x 8 :y 9)))
     (add-entity arena (make-instance 'entity
                                      :name "monster4"
                                      :team 'monster
                                      :abilities (list (make-ability))
                                      :display-char "M"
-                                     :position (list 8 8)))
+                                     :position (make-pos :x 8 :y 8)))
+
+    (setf (turn-order arena) '(hero monster))
+    (add-behavior arena 'hero #'null-behavior)
+    (add-behavior arena 'monster #'move-down-behavior)
     arena))
+
+(defun get-team-entities (arena team)
+ (mapcan #'(lambda (ent) (if (eq team (team ent)) (list ent))) (entities arena)))
+
+(defun move-down-behavior (arena team)
+  (let ((teammates (get-team-entities arena team))
+        (result-list nil))
+    (dolist (ent teammates result-list)
+      (setf result-list (acons (name ent) (list 'move 'down) result-list)))))
+
+(defun null-behavior (arena team)
+  nil)
+
+(defun get-entity-by-name (arena name)
+  (loop for ent in (entities arena)
+       if (equal (name ent) name) return ent))
+
+(defun resolve-turn (arena)
+  (dolist (team (turn-order arena) arena)
+    (format t "resolving turn for ~a~%" team)
+    (let ((behavior-cons (assoc team (behaviors arena))))
+      (if (not behavior-cons)
+          (format t "Team ~a does not have a behavior. Skipping...~%" team)
+          (dolist (action (funcall (cdr behavior-cons) arena team))
+            (format t "resolving action ~a~%" action)
+            (let ((entity (get-entity-by-name arena (car action)))
+                  (a (cdr action)))
+              (cond ((eq (first a) 'move)
+                     (case (second a) ; this eventually needs bounds checking
+                       (up (incf (pos-y (pos entity))))
+                       (down (decf (pos-y (pos entity))))
+                       (left (decf (pos-x (pos entity))))
+                       (right (incf (pos-x (pos entity))))))
+                    (t (format t "Team ~a Entity ~a: unknown action ~a~%"
+                               team (name entity) a)))))))))
