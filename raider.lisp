0cd1ea173786f207402978747d5e0a4dee2e29f0
Zavire Shiran <zavire.shiran@gmail.com>
2015-03-21 18:37:37 -0500

    Add ability usage and move calculation.

--- raider.lisp
+++ raider.lisp
@@ -17,7 +17,8 @@
   ((passable :accessor passable :initarg :passable :initform t)))
 
 (defclass arena ()
-  ((terrain :accessor terrain :initarg :terrain)
+  ((current-turn :accessor current-turn :initform 0)
+   (terrain :accessor terrain :initarg :terrain)
    (entities :accessor entities :initarg :entities :initform nil)
    (behaviors :accessor behaviors :initarg :behaviors :initform nil)
    (turn-order :accessor turn-order :initarg :turn-order :initform nil)))
@@ -53,7 +54,8 @@
             (setf entity-here entity))))))
 
 (defun display-arena (arena)
-  (with-slots (terrain entities) arena
+  (with-slots (terrain entities current-turn) arena
+    (format t "Turn ~a~%" current-turn)
     (destructuring-bind (xsize ysize) (array-dimensions terrain)
       (loop for y from (- ysize 1) downto 0
            do (loop for x from 0 to (- xsize 1)
@@ -63,34 +65,41 @@
                                      (format t "A")))
                               ((passable (aref terrain x y)) (format t "."))
                               (t (format t "O")))))
-              (format t "~%")))))
+              (format t "~%")))
+    (dolist (entity (entities arena))
+      (format t "(~a ~a) ~a ~a ~a~%" (pos-x (pos entity)) (pos-y (pos entity))
+              (name entity) (team entity) (stats entity)))))
 
 (defun make-ability ()
-  (make-instance 'ability :name "attack" :range 1 :effect '((hp-damage . 1))))
+  (make-instance 'ability :name "attack" :range 'touch :effect '((hp-damage . 1))))
 
-(defun make-test-arena ()
+(defun make-move-test-arena ()
   (let ((arena (make-arena '(10 10))))
     (add-entity arena (make-instance 'entity
                                      :name "hero1"
                                      :team 'hero
+                                     :stats (make-stats)
                                      :position (make-pos :x 0 :y 0)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero2"
                                      :team 'hero
+                                     :stats (make-stats)
                                      :position (make-pos :x 1 :y 0)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero3"
                                      :team 'hero
+                                     :stats (make-stats)
                                      :position (make-pos :x 0 :y 1)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
     (add-entity arena (make-instance 'entity
                                      :name "hero4"
                                      :team 'hero
+                                     :stats (make-stats)
                                      :position (make-pos :x 1 :y 1)
                                      :abilities (list (make-ability))
                                      :display-char "H"))
@@ -98,24 +107,28 @@
     (add-entity arena (make-instance 'entity
                                      :name "monster1"
                                      :team 'monster
+                                     :stats (make-stats)
                                      :abilities (list (make-ability))
                                      :display-char "M"
                                      :position (make-pos :x 9 :y 9)))
     (add-entity arena (make-instance 'entity
                                      :name "monster2"
                                      :team 'monster
+                                     :stats (make-stats)
                                      :abilities (list (make-ability))
                                      :display-char "M"
                                      :position (make-pos :x 9 :y 8)))
     (add-entity arena (make-instance 'entity
                                      :name "monster3"
                                      :team 'monster
+                                     :stats (make-stats)
                                      :abilities (list (make-ability))
                                      :display-char "M"
                                      :position (make-pos :x 8 :y 9)))
     (add-entity arena (make-instance 'entity
                                      :name "monster4"
                                      :team 'monster
+                                     :stats (make-stats)
                                      :abilities (list (make-ability))
                                      :display-char "M"
                                      :position (make-pos :x 8 :y 8)))
@@ -123,6 +136,50 @@
     (setf (turn-order arena) '(hero monster))
     (add-behavior arena 'hero #'null-behavior)
     (add-behavior arena 'monster #'move-down-behavior)
+    arena))
+
+(defun make-stats ()
+  (list (cons "hp" 20) (cons "move" 5)))
+
+(defun get-stat (entity stat)
+  (let ((stat-entry (assoc stat (stats entity) :test #'equal)))
+    (if stat-entry
+        (cdr stat-entry)
+        nil)))
+
+(defun make-attack-test-arena ()
+  (let ((arena (make-arena (list 10 10))))
+    (add-entity arena (make-instance 'entity
+                                     :name "hero1"
+                                     :team 'hero
+                                     :stats (make-stats)
+                                     :abilities (list (make-ability))
+                                     :display-char "H"
+                                     :position (make-pos :x 4 :y 4)))
+    (add-entity arena (make-instance 'entity
+                                     :name "hero2"
+                                     :team 'hero
+                                     :stats (make-stats)
+                                     :abilities (list (make-ability))
+                                     :display-char "H"
+                                     :position (make-pos :x 5 :y 5)))
+    (add-entity arena (make-instance 'entity
+                                     :name "monster1"
+                                     :team 'monster
+                                     :stats (make-stats)
+                                     :abilities (list (make-ability))
+                                     :display-char "M"
+                                     :position (make-pos :x 5 :y 4)))
+    (add-entity arena (make-instance 'entity
+                                     :name "monster2"
+                                     :team 'monster
+                                     :stats (make-stats)
+                                     :abilities (list (make-ability))
+                                     :display-char "M"
+                                     :position (make-pos :x 4 :y 5)))
+    (setf (turn-order arena) '(hero monster))
+    (add-behavior arena 'hero #'hero-attack-test-behavior)
+    (add-behavior arena 'monster #'monster-attack-test-behavior)
     arena))
 
 (defun get-team-entities (arena team)
@@ -133,6 +190,14 @@
         (result-list nil))
     (dolist (ent teammates result-list)
       (setf result-list (acons (name ent) (list 'move 'down) result-list)))))
+
+(defun hero-attack-test-behavior (arena team)
+  (list (list "hero1" 'use-ability "attack" 'up)
+        (list "hero2" 'use-ability "attack" 'down)))
+
+(defun monster-attack-test-behavior (arena team)
+  (list (list "monster1" 'use-ability "attack" 'right)
+        (list "monster2" 'use-ability "attack" 'left)))
 
 (defun null-behavior (arena team)
   nil)
@@ -146,7 +211,7 @@
   (let ((entity (get-entity-by-name arena (car action)))
         (a (cdr action)))
     (cond ((eq (first a) 'move)
-           (case (second a) ; this eventually needs bounds checking
+           (case (second a) ; this eventually needs bounds and terrain checking
              (up (incf (pos-y (pos entity))))
              (down (decf (pos-y (pos entity))))
              (left (decf (pos-x (pos entity))))
@@ -161,4 +226,39 @@
       (if (not behavior-cons)
           (format t "Team ~a does not have a behavior. Skipping...~%" team)
           (dolist (action (funcall (cdr behavior-cons) arena team))
-            (resolve-action arena team action))))))
+            (resolve-action arena team action)))))
+  (incf (current-turn arena)))
+
+(defun generate-neighbors (pos)
+  (list (make-pos :x (1+ (pos-x pos)) :y (pos-y pos))
+        (make-pos :x (1- (pos-x pos)) :y (pos-y pos))
+        (make-pos :x (pos-x pos) :y (1+ (pos-y pos)))
+        (make-pos :x (pos-x pos) :y (1- (pos-y pos)))))
+
+(defun bounds-check (arena pos)
+  (destructuring-bind (maxx maxy) (array-dimensions (terrain arena))
+    (let ((x (pos-x pos))
+          (y (pos-y pos)))
+      (and (>= x 0) (>= y 0) (< x maxx) (< y maxy)))))
+
+(defun pos-equal (pos1 pos2)
+  (and (= (pos-x pos1) (pos-x pos2))
+       (= (pos-y pos1) (pos-y pos2))))
+
+(defun find-possible-moves (arena entity)
+  (let* ((entitypos (pos entity))
+         (move (get-stat entity "move"))
+         (possible-moves nil)
+         (move-queue (list (cons entitypos 0))))
+    (do ((pos (car (first move-queue)) (car (first move-queue)))
+         (current-moves (cdr (first move-queue)) (cdr (first move-queue))))
+        ((not move-queue) possible-moves)
+      (pop move-queue)
+      (when (and (bounds-check arena pos)
+                 (passable (aref (terrain arena) (pos-x pos) (pos-y pos)))
+                 (not (find pos possible-moves :test #'pos-equal)))
+        (push pos possible-moves)
+        (when (< current-moves move)
+          (setf move-queue (nconc move-queue (mapcar #'cons
+                                                     (generate-neighbors pos)
+                                                     (make-list 8 :initial-element (1+ current-moves))))))))))
